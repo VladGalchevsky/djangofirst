bdc48372872d2effac5af3449e95b011a0a02a3c
VladGalchevsky <vladgalchevsky97@gmail.com>
2022-05-15 21:12:31 +0300

    Add students app 3

--- students/views.py
+++ students/views.py
@@ -3,7 +3,24 @@ from django.http import HttpResponse
 
 # Views for Students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+       {'id': 1,
+        'first_name': 'Владислав',
+        'last_name': 'Літвінов',
+        'ticket': 212,
+        'image': 'img/vlad.jpg'},
+       {'id': 2,
+        'first_name': 'Андрій',
+        'last_name': 'Подоба',  
+        'ticket': 213,
+        'image': 'img/andrew.jpeg'},
+       {'id': 3,
+        'first_name': 'Ігор',
+        'last_name': 'Дорощук',
+        'ticket': 214,
+        'image': 'img/igor.jpeg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
